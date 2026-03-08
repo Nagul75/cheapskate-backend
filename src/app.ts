7c36fe5637edb86fc24192e5699ff56e4ea94d5c
Nagul75 <nagulk697@gmail.com>
2026-03-08 15:59:47 +0530

    configure cors for frontend

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,10 @@ import morgan from "morgan";
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));

fe27bf8a2c68f672e2dc5c95c5ba4202d1ca0318
Nagul75 <nagulk697@gmail.com>
2026-03-21 10:56:25 +0530

    CS-19 update cors and add rate limiter

--- src/app.ts
+++ src/app.ts
@@ -6,19 +6,24 @@ import apiRouter from "./routes/ApiRouter";
 import cookieParser from "cookie-parser";
 import "./types/auth";
 import morgan from "morgan";
+import "dotenv/config";
+import { apiLimiter, authLimiter } from "./config/rateLimiter";
 
 const app = express();
 
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: [
+    "https://cheapskate.in",
+    "https://www.cheapskate.in"
+  ],
     credentials: true
 }));
 app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
-app.use("/auth", authRouter);
-app.use("/api", apiRouter);
+app.use("/auth", authLimiter, authRouter);
+app.use("/api", apiLimiter, apiRouter);
 
 export default app;

02341a5163903d083e42e5f78b7e4ad3e1f0ed90
Nagul75 <nagulk697@gmail.com>
2026-03-21 10:57:10 +0530

    CS-19 update db export to use pool

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,11 +1,14 @@
 import "dotenv/config";
+import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client";
-import "dotenv/config"
 
-const connectionString = process.env.DATABASE_URL 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
